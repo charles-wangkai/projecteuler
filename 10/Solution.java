1eaccf02543827892f46763949b0e6f11bada4d7
Kai Wang <charles.wangkai@gmail.com>
2022-01-21 19:24:44 +0800

    Summation of primes

---
+++ 10/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Solution {
+  public static void main(String[] args) {
+    assert solve(10) == 17;
+
+    System.out.println(solve(2_000_000));
+  }
+
+  static long solve(int limit) {
+    boolean[] primes = new boolean[limit];
+    Arrays.fill(primes, true);
+
+    for (int i = 2; i < primes.length; ++i) {
+      if (primes[i]) {
+        for (long j = (long) i * i; j < primes.length; j += i) {
+          primes[(int) j] = false;
+        }
+      }
+    }
+
+    return IntStream.range(2, primes.length).filter(i -> primes[i]).asLongStream().sum();
+  }
+}
