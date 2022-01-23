cba90771cb732b51f71c88387014df113137be5b
Kai Wang <charles.wangkai@gmail.com>
2022-01-23 16:32:37 +0800

    Number letter counts

--- 17/Solution.java
+++ 17/Solution.java
@@ -21,18 +21,22 @@ public class Solution {
   };
 
   public static void main(String[] args) {
-    assert solve(1) == 3;
-    assert solve(2) == 3;
-    assert solve(3) == 5;
-    assert solve(4) == 4;
-    assert solve(5) == 4;
-    assert solve(342) == 23;
-    assert solve(115) == 20;
-
-    System.out.println(IntStream.rangeClosed(1, 1000).map(Solution::solve).sum());
+    System.out.println(solve());
   }
 
-  static int solve(int n) {
+  static int solve() {
+    assert computeLetterNum(1) == 3;
+    assert computeLetterNum(2) == 3;
+    assert computeLetterNum(3) == 5;
+    assert computeLetterNum(4) == 4;
+    assert computeLetterNum(5) == 4;
+    assert computeLetterNum(342) == 23;
+    assert computeLetterNum(115) == 20;
+
+    return IntStream.rangeClosed(1, 1000).map(Solution::computeLetterNum).sum();
+  }
+
+  static int computeLetterNum(int n) {
     if (n == 0) {
       return 0;
     } else if (n <= 9) {
@@ -40,12 +44,12 @@ public class Solution {
     } else if (n <= 19) {
       return TEENS[n - 10].length();
     } else if (n <= 99) {
-      return TENS[n / 10 - 2].length() + solve(n % 10);
+      return TENS[n / 10 - 2].length() + computeLetterNum(n % 10);
     } else if (n <= 999) {
       return ONES[n / 100 - 1].length()
           + "hundred".length()
           + ((n % 100 == 0) ? 0 : "and".length())
-          + solve(n % 100);
+          + computeLetterNum(n % 100);
     }
 
     return ONES[0].length() + "thousand".length();
