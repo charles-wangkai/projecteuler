9ff744e78edc0ac84d1875586d757fbcf7c8aa77
Kai Wang <charles.wangkai@gmail.com>
2022-01-23 16:25:48 +0800

    Longest Collatz sequence

--- 14/Solution.java
+++ 14/Solution.java
@@ -7,25 +7,27 @@ public class Solution {
   static Map<Long, Integer> cache = new HashMap<>();
 
   public static void main(String[] args) {
-    assert solve(13) == 10;
-
-    System.out.println(
-        IntStream.rangeClosed(1, 1_000_000)
-            .asLongStream()
-            .boxed()
-            .max(Comparator.comparing(Solution::solve))
-            .get());
+    System.out.println(solve());
   }
 
-  static int solve(long n) {
+  static int solve() {
+    assert computeChainLength(13) == 10;
+
+    return IntStream.rangeClosed(1, 1_000_000)
+        .boxed()
+        .max(Comparator.comparing(i -> computeChainLength(i)))
+        .get();
+  }
+
+  static int computeChainLength(long n) {
     if (!cache.containsKey(n)) {
       int result;
       if (n == 1) {
         result = 1;
       } else if (n % 2 == 0) {
-        result = 1 + solve(n / 2);
+        result = 1 + computeChainLength(n / 2);
       } else {
-        result = 1 + solve(3 * n + 1);
+        result = 1 + computeChainLength(3 * n + 1);
       }
 
       cache.put(n, result);
